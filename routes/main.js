9eaa79933d29794227ee9e742714d3a98a2547bc
unknown <xm@geniusnet.com.tw>
2018-10-27 22:01:21 +0800

    get html

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var reqData = require('../template/requestUrl');
+var getHtmlClass = require('../template/requestUrl');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let tmp = reqData.getreqData();
-  console.log(tmp);
+  let a = getHtmlClass.getbodyData();
+  console.log("tmp");
 });
 
 module.exports = router;
--- template/requestUrl.js
+++ template/requestUrl.js
@@ -1,15 +1,43 @@
 var request= require('request');
+var cheerio=require('cheerio');
+var iconv = require('iconv-lite');
 
 let haoUrl = "http://www.hao6v.com/"
 
+const options = {
+    url: haoUrl,
+    method: 'GET',
+    encoding: null
+};
 
-getreqData = ()=>{
-    request(haoUrl,(err,body,connt)=>{
-        console.log(body.body);
-    })
+class getHtml{
+    getreqData(){
+        return new Promise((resolve,reject)=>{
+            request(options,(err,res)=>{
+                if(err){
+                    reject(err);
+                }else{
+                    var buf = iconv.decode(res.body,'gb2312');
+                    resolve(buf);
+                }
+            });
+        })
+    };
 
-
-return "aaaaa";
+    getbodyData(){
+        this.getreqData().then((res)=>{
+           let tmpdata = cheerio.load(res);
+           let $ = cheerio.load(tmpdata);
+           let bodys = $('<div id="menu">...</div>');
+           console.log(bodys.html());
+        }).catch((err)=>{
+            return err;
+        });
+     };
+}
+if(!getHtmlClass){
+    var getHtmlClass = new getHtml();
 }
 
-exports.getreqData = getreqData;
+module.exports = getHtmlClass;
+
